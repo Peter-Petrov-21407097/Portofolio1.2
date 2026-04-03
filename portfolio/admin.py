5f50a88f3ed835bdd70ab4bbe5f020e254ce8695
Pedro-Santos-a21407097 <pedro_s9@outlook.com>
2026-04-03 12:07:13 +0000

    Criado modelo Docente. Adicionado Professora Maria Silva

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 
-from .models import Licenciatura, UnidadeCurricular
+from .models import Licenciatura, UnidadeCurricular, Docente
 
 
 @admin.register(Licenciatura)
@@ -15,3 +15,10 @@ class UnidadeCurricularAdmin(admin.ModelAdmin):
     list_display = ('nome', 'licenciatura', 'ano_curricular', 'semestre', 'ects', 'url_website')
     search_fields = ('nome', 'apresentacao', 'programa', 'objectivos')
     list_filter = ('licenciatura', 'ano_curricular', 'semestre', 'ects')    
+
+@admin.register(Docente)
+class DocenteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'area_especializacao', 'pagina_pessoal_url')
+    search_fields = ('nome', 'email', 'area_especializacao')
+    list_filter = ('area_especializacao',)
+    filter_horizontal = ('unidades_curriculares',)
--- portfolio/models.py
+++ portfolio/models.py
@@ -13,4 +13,38 @@ class Licenciatura(models.Model):
 
     def __str__(self):
         return self.nome
+
+
+class UnidadeCurricular(models.Model):
+    licenciatura = models.ForeignKey(
+        Licenciatura,
+        on_delete=models.CASCADE,
+        related_name='unidades_curriculares'
+    )
+    nome = models.CharField(max_length=200)
+    ano_curricular = models.PositiveIntegerField()
+    semestre = models.PositiveIntegerField()
+    ects = models.PositiveIntegerField()
+    apresentacao = models.TextField()
+    programa = models.TextField()
+    objectivos = models.TextField()
+    imagem = models.ImageField(upload_to='unidades_curriculares/', blank=True, null=True)
+    url_website = models.URLField()
+
+    def __str__(self):
+        return self.nome
     
+class Docente(models.Model):
+    nome = models.CharField(max_length=200)
+    pagina_pessoal_url = models.URLField()
+    email = models.EmailField()
+    area_especializacao = models.CharField(max_length=200)
+    foto = models.ImageField(upload_to='docentes/', blank=True, null=True)
+    unidades_curriculares = models.ManyToManyField(
+        UnidadeCurricular,
+        related_name='docentes',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.nome    
